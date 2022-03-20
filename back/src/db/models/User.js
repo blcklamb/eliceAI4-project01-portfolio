56c66c96edc09f6acb5c0139b4c41744e7aebdc4
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-20 12:02:57 +0900

    Revert "Merge branch 'feature/userImageUpload-front' into 'main'"
    This reverts commit a29191cc6d10639a3aa871b6620e2ff1e22eefdf, reversing
    changes made to a54dfdbb4dea5b393de151ead4eca67a626fa47e.

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -1,47 +1,38 @@
-import { UserModel } from "../schemas/User";
+import { UserModel } from "../schemas/user";
 
 class User {
-    static async create({ newUser }) {
-        const createdNewUser = await UserModel.create(newUser);
-        return createdNewUser;
-    }
+  static async create({ newUser }) {
+    const createdNewUser = await UserModel.create(newUser);
+    return createdNewUser;
+  }
 
-    static async findOne({ email }) {
-        const user = await UserModel.findOne({ email });
-        return user;
-    }
+  static async findByEmail({ email }) {
+    const user = await UserModel.findOne({ email });
+    return user;
+  }
 
-    static async findById({ user_id }) {
-        const user = await UserModel.findOne({ id: user_id });
-        return user;
-    }
+  static async findById({ user_id }) {
+    const user = await UserModel.findOne({ id: user_id });
+    return user;
+  }
 
-    static async findAll({ perPage, page }) {
-        const users = await UserModel.find({})
-            .sort({ created: -1 })
-            .skip(perPage * (page - 1))
-            .limit(perPage);
-        return users;
-    }
+  static async findAll() {
+    const users = await UserModel.find({});
+    return users;
+  }
 
-    static async update({ user_id, fieldToUpdate, newValue }) {
-        const filter = { id: user_id };
-        const update = { [fieldToUpdate]: newValue };
-        const option = { returnOriginal: false };
+  static async update({ user_id, fieldToUpdate, newValue }) {
+    const filter = { id: user_id };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
 
-        const updatedUser = await UserModel.findOneAndUpdate(filter, update, option);
-        return updatedUser;
-    }
-
-    static async resetPassword({ user_id }, { password }) {
-        const updatedPassword = await UserModel.findOneAndUpdate({ id: user_id }, { password });
-        return updatedPassword;
-    }
-
-    static async countDocuments() {
-        const usersCount = await UserModel.countDocuments({});
-        return usersCount;
-    }
+    const updatedUser = await UserModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedUser;
+  }
 }
 
 export { User };
